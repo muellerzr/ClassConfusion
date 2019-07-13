1599dab8bc0536b322154169b64f80079a18f4a4
Zachary Mueller <muellerzr@gmail.com>
2019-07-13 14:45:53 -0500

    Fixed graphing issues!

--- classLosses.py
+++ classLosses.py
@@ -14,7 +14,7 @@ class ClassLosses():
     if str(type(interp.learn.data)) == "<class 'fastai.tabular.data.TabularDataBunch'>":
       self.means = interp.learn.data.train_ds.x.processor[0].procs[2].means
       self.stds = interp.learn.data.train_ds.x.processor[0].procs[2].stds
-    self.show_losses(classlist)
+    self.classlist = classlist
     
   def create_graphs(self, df_list:list, cat_names:list):
     cols = math.ceil(math.sqrt(len(df_list)))
@@ -42,10 +42,8 @@ class ClassLosses():
             vals = vals * self.stds[tab] + self.means[tab]
             ttl = str.join('', df_list[j].columns[0])
             
-            k = min(vals, key = lambda t: t[1])
-            l = max(vals, key = lambda t: t[1])
-            fig = vals.plot(kind='hist', normed=True, title= ttl + ' ' + tbnames[i] +' distrobution', 
-                            rot=30, ax=ax[j], range=[int(k), int(l)])
+            axs = vals.plot(kind='hist', ax=ax[j])
+            vals.plot(kind='kde', ax=axs, secondary_y=True)
             
         plt.tight_layout()
   
@@ -59,7 +57,8 @@ class ClassLosses():
       arr1 = []
       comb = list(permutations(classl,2))
       for j, x in enumerate(comb):
-        df = pd.DataFrame(columns=[[str(x)] + cat_names + cont_names])
+        print(comb)
+        df = pd.DataFrame(columns=[[str(comb[j])] + cat_names + cont_names])
         for i, idx in enumerate(tl_idx):
           da, cl = self.interp.data.dl(self.interp.ds_type).dataset[idx]
           cl = int(cl)
@@ -67,61 +66,14 @@ class ClassLosses():
           t1 = t1.split(';')
           if classes[self.interp.pred_class[idx]] == comb[j][0] and classes[cl] == comb[j][1]:
             arr = []
-            arr.append(x)
+            arr.append(str(comb[j]))
             for x in range(len(t1)-1):
                 _, value = t1[x].rsplit(' ', 1)
                 arr.append(value)
             df.loc[i] = arr
         arr1.append(df)
-      self.create_graphs(arr1, cat_names)
+      self.arr = arr1
+      self.cat_names = cat_names
+      
+ 
     
-    else:
-      comb = list(permutations(classl, 2))
-      tl_val, tl_idx = self.interp.top_losses(len(self.interp.losses))
-      
-      classes_gnd = self.interp.data.classes
-      vals = self.interp.most_confused()
-      ranges = []
-      tbnames = []
-      
-      k = input('Please enter a value for `k`: ')
-      k = int(k)
-      
-      for x in iter(vals):
-        for y in iter(comb):
-          if x[0:2] == y:
-            ranges.append(x[2])
-            tbnames.append(str(x[0] + ' | ' + x[1]))
-      
-      tb = widgets.TabBar(tbnames)
-      
-      for i, tab in enumerate(tbnames):
-        with tb.output_to(i):
-          
-          x = 0          
-          if ranges[i] < k:
-            cols = math.ceil(math.sqrt(ranges[i]))
-            rows = math.ceil(ranges[i]/cols)
-          
-          if ranges[i] < 4:
-            cols, rows = 2, 2
-          
-          else:
-            cols = math.ceil(math.sqrt(k))
-            rows = math.ceil(k/cols)
-          
-          fig, axes = plt.subplots(rows, cols, figsize=(8,8))
-          [axi.set_axis_off() for axi in axes.ravel()]
-          for j, idx in enumerate(tl_idx):
-            if k < x+1 or x > ranges[i]:
-              break
-            da, cl = self.interp.data.dl(self.interp.ds_type).dataset[idx]
-            row = (int)(x / cols)
-            col = x % cols
-
-            ix = int(cl)
-            if str(cl) == tab.split(' ')[0] and str(classes_gnd[self.interp.pred_class[idx]]) == tab.split(' ')[2]:
-              da = image2np(da.data*255).astype(np.uint8)
-              axes[row, col].imshow(da)
-              x += 1
-          plt.tight_layout()
