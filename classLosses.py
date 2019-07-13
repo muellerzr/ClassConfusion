c28b8e0a55a465c30f9a8108aa848f600ce8bc0e
Zachary Mueller <muellerzr@gmail.com>
2019-07-13 13:42:28 -0500

    Get rid of seaborn

--- classLosses.py
+++ classLosses.py
@@ -5,7 +5,6 @@ import seaborn as sns
 
 from itertools import permutations
 from google.colab import widgets
-from seaborn import distplot, distributions
 from fastai.vision import ClassificationInterpretation
 
 class ClassLosses():
@@ -42,8 +41,9 @@ class ClassLosses():
             vals = df_list[j][tab].astype(float)
             vals = vals * self.stds[tab] + self.means[tab]
             ttl = str.join('', df_list[j].columns[0])
-            bins = int(min(distributions._freedman_diaconis_bins(vals), 50))
-            distplot(ax=ax[j],a=vals, bins=bins).set_title(ttl + ' ' + tbnames[i]+' distrobution')
+            
+            fig = vals.plot(kind='hist', normed=True, title= ttl + ' ' + tbnames[i]+' distrobution', rot=30, ax=ax[j])
+            
         plt.tight_layout()
   
   def show_losses(self, classl:list, **kwargs):
