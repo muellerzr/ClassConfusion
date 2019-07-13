8e5c9d206379ac79eb78ce243c8e166496bc1355
Zachary Mueller <muellerzr@gmail.com>
2019-07-12 19:19:50 -0500

    Update classLosses.py

--- classLosses.py
+++ classLosses.py
@@ -2,6 +2,7 @@ import math
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import pandas as pd
 
 from pandas import DataFrame, value_counts
 from itertools import permutations
