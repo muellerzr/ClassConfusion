a25bb2efd24421b993c22081c146c40f0d3e299c
Zachary Mueller <muellerzr@gmail.com>
2019-07-12 20:17:34 -0500

    Update classLosses.py

--- classLosses.py
+++ classLosses.py
@@ -61,56 +61,55 @@ class ClassLosses():
     else:
       k = input('Please enter a value for `k` ')
       k = int(k)
-      imgClassLosses(self.interp, k, classl)
+      self.imgClassLosses(self.interp, k, classl)
   
-def imgClassLosses(interp:ClassificationInterpretation, k:float, classes:list, **kwargs):
-    if ('figsize' in kwargs):
-        figsize = kwargs['figsize']
-    else:
-        figsize = (8,8)
-    comb = list(permutations(classes, 2))
-    val, idxs = interp.top_losses(len(interp.losses))
+  def imgClassLosses(interp:ClassificationInterpretation, k:float, classes:list, **kwargs):
+      if ('figsize' in kwargs):
+          figsize = kwargs['figsize']
+      else:
+          figsize = (8,8)
+      comb = list(permutations(classes, 2))
+      val, idxs = interp.top_losses(len(interp.losses))
 
-    cols = math.ceil(math.sqrt(k))
-    rows = math.ceil(k/cols)
+      cols = math.ceil(math.sqrt(k))
+      rows = math.ceil(k/cols)
 
-    classes_gnd = interp.data.classes
+      classes_gnd = interp.data.classes
 
-    vals = interp.most_confused()
-    ranges = []
-    tbnames = []
+      vals = interp.most_confused()
+      ranges = []
+      tbnames = []
 
-    
-    for x in iter(vals):
-      for y in iter(comb):
-        if x[0:2] == y:
-          ranges.append(x[2])
-          tbnames.append(str(x[0] + ' | ' + x[1])) 
-  
-    tb = widgets.TabBar(tbnames)
-        
-    for i, tab in enumerate(tbnames):
-      with tb.output_to(i):
-        x = 0
-        
-        if ranges[i] < k:
-          cols = math.ceil(math.sqrt(ranges[i]))
-          rows = math.ceil(k/cols)
-        if ranges[i] < 4:
-          cols = 2
-          rows = 2
-        fig, axes = plt.subplots(rows, cols, figsize=figsize)
-        [axi.set_axis_off() for axi in axes.ravel()]
-        for j, idx in enumerate(idxs):
-          if k < x+1 or x > ranges[i]:
-            break
-          da, cl = interp.data.dl(interp.ds_type).dataset[idx]
-          row = (int)(x / cols)
-          col = x % cols
+      for x in iter(vals):
+        for y in iter(comb):
+          if x[0:2] == y:
+            ranges.append(x[2])
+            tbnames.append(str(x[0] + ' | ' + x[1])) 
 
-          ix = int(cl)
-          if str(cl) == tab[0] and str(classes_gnd[interp.pred_class[idx]]) == tab[1]:
-            da = image2np(da.data*255).astype(np.uint8)
-            axes[row, col].imshow(da)
-            x += 1
-        plt.tight_layout()
+      tb = widgets.TabBar(tbnames)
+
+      for i, tab in enumerate(tbnames):
+        with tb.output_to(i):
+          x = 0
+
+          if ranges[i] < k:
+            cols = math.ceil(math.sqrt(ranges[i]))
+            rows = math.ceil(k/cols)
+          if ranges[i] < 4:
+            cols = 2
+            rows = 2
+          fig, axes = plt.subplots(rows, cols, figsize=figsize)
+          [axi.set_axis_off() for axi in axes.ravel()]
+          for j, idx in enumerate(idxs):
+            if k < x+1 or x > ranges[i]:
+              break
+            da, cl = interp.data.dl(interp.ds_type).dataset[idx]
+            row = (int)(x / cols)
+            col = x % cols
+
+            ix = int(cl)
+            if str(cl) == tab[0] and str(classes_gnd[interp.pred_class[idx]]) == tab[1]:
+              da = image2np(da.data*255).astype(np.uint8)
+              axes[row, col].imshow(da)
+              x += 1
+          plt.tight_layout()
