c13fd1ca8ec5bc47af400f28f132ac2d3af9b88d
Zachary Mueller <muellerzr@gmail.com>
2019-07-15 21:53:18 -0500

    Fixed tabular
    Fixed issue with the column names, also loaded the actual dataframes faster.

--- classLosses.py
+++ classLosses.py
@@ -1,14 +1,13 @@
 import math
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
 
 from itertools import permutations
 from google.colab import widgets
 from fastai.vision import ClassificationInterpretation
 
 class ClassLosses():
-  "Plot the most confused datapoints and statistics for your misses. \nPass in a `Learner` object and a list of classes to look at."
+  "Plot the most confused datapoints and statistics for your misses. \nPass in a `interp` object and a list of classes to look at."
   def __init__(self, interp:ClassificationInterpretation, classlist:list, is_ordered:bool=False):
     self.interp = interp
     if str(type(interp.learn.data)) == "<class 'fastai.tabular.data.TabularDataBunch'>":
@@ -20,6 +19,7 @@ class ClassLosses():
     
     
   def create_graphs(self, df_list:list, cat_names:list):
+    print('Variable Distrobution:')
     cols = math.ceil(math.sqrt(len(df_list)))
     rows = math.ceil(len(df_list)/cols)
     df_list[0].columns = df_list[0].columns.get_level_values(0)
@@ -28,7 +28,7 @@ class ClassLosses():
     tbnames = tbnames[1:]
     tb = widgets.TabBar(tbnames)
     
-    print('Variable Distrobution:')
+    
     for i, tab in enumerate(tbnames):
       with tb.output_to(i):
         
@@ -38,15 +38,23 @@ class ClassLosses():
           col = j % cols
           if tab in cat_names:
             vals = pd.value_counts(df_list[j][tab].values.flatten())
-            ttl = str.join('', df_list[j].columns[0])
-            fig = vals.plot(kind='bar', title= ttl + ' ' + tbnames[i]+' distrobution', rot=30, ax=ax[j])
+            ttl = str.join('', df_list[j].columns[-1])
+            if j == 0:
+              title = ttl + ' ' + tbnames[i]+' distrobution'
+            else:
+              title = 'Misclassified ' + ttl + ' ' + tbnames[i]+' distrobution'
+            fig = vals.plot(kind='bar', title= title, rot=30, ax=ax[j])
           else:
             vals = df_list[j][tab].astype(float)
             vals = vals * self.stds[tab] + self.means[tab]
-            ttl = str.join('', df_list[j].columns[0])
+            ttl = str.join('', df_list[j].columns[-1])
+            if j == 0:
+              title = ttl + ' ' + tbnames[i] + ' distrobution'
+            else:
+              title = 'Misclassified ' + ttl + ' ' + tbnames[i]+' distrobution'
             
             axs = vals.plot(kind='hist', ax=ax[j])
-            vals.plot(kind='kde', ax=axs, title = ttl + ' ' + tbnames[i]+' distrobution', secondary_y=True)
+            vals.plot(kind='kde', ax=axs, title = title, secondary_y=True)
             
         plt.tight_layout()
   
@@ -57,101 +65,65 @@ class ClassLosses():
       self.im_losses(classl)
       
       
+      
   def tab_losses(self, classl:list, **kwargs):
       tl_val, tl_idx = self.interp.top_losses(len(self.interp.losses))
       classes = self.interp.data.classes
       cat_names = self.interp.data.x.cat_names
       cont_names = self.interp.data.x.cont_names
-      arr1 = []
       if self.is_ordered == False:
         comb = list(permutations(classl,2))
       else:
         comb = classl
       
-      df = pd.DataFrame(columns=[['Original'] + cat_names + cont_names])
+      dfarr = []
+      
+      arr = []
       for i, idx in enumerate(tl_idx):
-        da, cl = self.interp.data.dl(self.interp.ds_type).dataset[idx]
-        cl = int(cl)
-        tl = str(da)
-        tl = tl.split(';')
-        arr = []
-        arr.append('Original')
-        for x in range(len(tl)-1):
-          _, value = tl[x].rsplit(' ', 1)
-          arr.append(value)
-        df.loc[i] = arr
-      arr1.append(df)
-      
-      for j, x in enumerate(comb):
-        df = pd.DataFrame(columns=[[str(comb[j])] + cat_names + cont_names])
-        for i, idx in enumerate(tl_idx):
-          da, cl = self.interp.data.dl(self.interp.ds_type).dataset[idx]
-          cl = int(cl)
-          t1 = str(da)
-          t1 = t1.split(';')
-          if classes[self.interp.pred_class[idx]] == comb[j][0] and classes[cl] == comb[j][1]:
-            arr = []
-            arr.append(str(comb[j]))
-            for x in range(len(t1)-1):
-                _, value = t1[x].rsplit(' ', 1)
-                arr.append(value)
-            df.loc[i] = arr
-        arr1.append(df)
-      self.create_graphs(arr1, cat_names)
-      
-  def im_losses(self, classl:list, **kwargs):
-      if self.is_ordered == False:
-        comb = list(permutations(classl, 2))
-      else:
-        comb = classl
-      tl_val, tl_idx = self.interp.top_losses(len(self.interp.losses))
-
-      classes_gnd = self.interp.data.classes
-      vals = self.interp.most_confused()
-      ranges = []
-      tbnames = []
-
-      k = input('Please enter a value for `k`: ')
-      k = int(k)
-
-      for x in iter(vals):
-        for y in iter(comb):
-          if x[0:2] == y:
-            ranges.append(x[2])
-            tbnames.append(str(x[0] + ' | ' + x[1]))
-      print('Misclassified Pictures:')
-      tb = widgets.TabBar(tbnames)
-
-      for i, tab in enumerate(tbnames):
-        with tb.output_to(i):
-
-          x = 0          
-          if ranges[i] < k:
-            cols = math.ceil(math.sqrt(ranges[i]))
-            rows = math.ceil(ranges[i]/cols)
-
-          if ranges[i] < 4:
-            cols, rows = 2, 2
+        da, _ = self.interp.data.dl(self.interp.ds_type).dataset[idx]
+        res = ''
+        for c, n in zip(da.cats, da.names[:len(da.cats)]):
+          string = f'{da.classes[n][c]}'
+          if string == 'True' or string == 'False':
+            string += ';'
+            res += string
 
           else:
-            cols = math.ceil(math.sqrt(k))
-            rows = math.ceil(k/cols)
-
-          fig, axes = plt.subplots(rows, cols, figsize=(8,8))
-          [axi.set_axis_off() for axi in axes.ravel()]
-          for j, idx in enumerate(tl_idx):
-            if k < x+1 or x > ranges[i]:
-              break
-            da, cl = self.interp.data.dl(self.interp.ds_type).dataset[idx]
-            row = (int)(x / cols)
-            col = x % cols
-
-            ix = int(cl)
-            if str(cl) == tab.split(' ')[0] and str(classes_gnd[self.interp.pred_class[idx]]) == tab.split(' ')[2]:
-              img, lbl = data.valid_ds[idx]
-              img.show(ax=axes[row,col])
-              fn = self.interp.data.valid_ds.x.items[idx]
-              fn = re.search('([^/*]+)_\d+.*$', str(fn)).group(0)
-              axes[row, col].set_title(fn)
-              x += 1
-          plt.tight_layout()
+            string = string[1:]
+            res += string + ';'
+        for c, n in zip(da.conts, da.names[len(da.cats):]):
+          res += f'{c:.4f};'
+        arr.append(res)
+      f = pd.DataFrame([ x.split(';')[:-1] for x in arr], columns=da.names)
+      for i, var in enumerate(self.interp.data.cont_names):
+        f[var] = f[var].apply(lambda x: float(x) * norm.stds[var] + norm.means[var])
+      f['Original'] = 'Original'
+      dfarr.append(f)
+      
+      
+      for j, x in enumerate(comb):
+        arr = []
+        for i, idx in enumerate(tl_idx):
+          da, cl = interp.data.dl(interp.ds_type).dataset[idx]
+          cl = int(cl)
+          
+          if classes[interp.pred_class[idx]] == comb[j][0] and classes[cl] == comb[j][1]:
+            res = ''
+            for c, n in zip(da.cats, da.names[:len(da.cats)]):
+              string = f'{da.classes[n][c]}'
+              if string == 'True' or string == 'False':
+                string += ';'
+                res += string
+              else:
+                string = string[1:]
+                res += string + ';'
+            for c, n in zip(da.conts, da.names[len(da.cats):]):
+              res += f'{c:.4f};'
+            arr.append(res)      
+        f = pd.DataFrame([ x.split(';')[:-1] for x in arr], columns=da.names)
+        for i, var in enumerate(self.interp.data.cont_names):
+          f[var] = f[var].apply(lambda x: float(x) * norm.stds[var] + norm.means[var])
+        f[str(x)] = str(x)
+        dfarr.append(f)
+      
+      self.create_graphs(dfarr, cat_names)
